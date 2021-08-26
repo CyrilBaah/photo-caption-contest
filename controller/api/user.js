4864b022bbefc61978c89c16b2fbaaeb5deb52b0
CyrilBaah <cyril.baah@amalitech.org>
2021-08-26 14:03:06 +0000

    Delete a user

--- controller/api/user.js
+++ controller/api/user.js
@@ -41,3 +41,18 @@ exports.updateUser = async (req, res) => {
         res.status(400).json({ success: false, message: error });
     }
 }
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findOne({ where: { id } });
+        if(user) {
+            user.destroy();
+            return res.status(204).json({ success: true, message: user });
+        }
+        return res.status(400).json({ success: false, message: `User with id:${id} doesn't exist` });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ success: false, message: error });
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const userController = require('../controller/api/user');
 router.post('/api/users', userController.createUser);
 router.get('/api/users', userController.getUsers);
 router.put('/api/users/:id', userController.updateUser);
+router.delete('/api/users/:id', userController.deleteUser);
+
 
 
 module.exports = router;
